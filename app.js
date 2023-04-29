8d8c806332aba9749cee557825d7fb9e78aa6214
jihyeona <jihyunan42@gmail.com>
2023-04-29 22:30:42 +0200

    set up firebase

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+require('dotenv').config();
+
 const express = require('express')
+const admin = require('firebase-admin');
+
+const serviceAccount = require(process.env.FIREBASE_KEY_PATH);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://text-service-c6351-default-rtdb.europe-west1.firebasedatabase.app/'
+});
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    const database = admin.database();
+    const ref = database.ref('test');
+    ref.set('Hello, world again!').then(() => {
+        res.send('Firebase initialised successfully!')
+    }).catch(error => {
+        console.error('Error writing to Firebase:', error);
+        res.status(500).send('Error writing to Firebase');
+    });
 })
 
 app.listen(port, () => {
