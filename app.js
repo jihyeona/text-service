cad43bdd67243998697fdb9c1df4f42266f05fe8
jihyeona <jihyunan42@gmail.com>
2023-05-06 17:27:23 +0200

    parse json for firebase secret file

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const admin = require("firebase-admin");
 
-const serviceAccount = require(process.env.FIREBASE_KEY_PATH);
+const fs = require("fs");
+
+const serviceAccount = JSON.parse(fs.readFileSync(process.env.FIREBASE_KEY_PATH));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
